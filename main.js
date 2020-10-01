52072963834f18f967d56fe73b84fd425a842246
Sol Zilberman <solzilbermancode@gmail.com>
2020-09-30 20:51:35 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+const http = require('http');
+var app = express();
+var axios = require('axios');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+const accountSid = 'AC6330ccfc0f2fcb429b1e40548487d534';
+const authToken = 'ac6e59eacf97573a429d1dc13c21758e';
+const client = require('twilio')(accountSid, authToken);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/message", function (request, response) {
+
+    console.log(request.body.Body);
+    var food = request.body.Body
+    //response.send("<Response><Message>Hello</Message></Response>")
+    if (food != "!list") {
+        let res_string = "Added " + food + " to your list! 🍔"
+        var date = new Date();
+        let url = 'https://timeblocker-4dede.firebaseio.com/food_list_' + date.toDateString() + '.json'
+        client.messages
+            .create({
+                body: res_string,
+                from: '+12055743043',
+                to: '+18472085541'
+            })
+            .then(message => axios.post(url, { "food": food }).then(res => console.log("added to db")));
+    } else {
+        let res_string = "Here are todays groceries..."
+        var date = new Date();
+        // let url = 'https://timeblocker-4dede.firebaseio.com/food_list_' + date.toDateString() + '.json'
+        axios.get("https://timeblocker-4dede.firebaseio.com/food_list_Wed%20Sep%2030%202020.json")
+            .then(res => {
+                let gs = Object.values(res.data);
+                let ls = "";
+                for (let i = 0; i < gs.length; i++) {
+                    ls += " " + gs[i].food
+                }
+                console.log(ls)
+                let ret_s = "Here is todays list: " + ls
+                client.messages
+                    .create({
+                        body: ret_s,
+                        from: '+12055743043',
+                        to: '+18472085541'
+                    })
+
+            })
+    }
+
+
+});
+
+
+
+
+app.get("/", function (request, response) {
+    response.sendFile(__dirname + '/views/index.html');
+});
+const PORT = 3333;
+http.createServer(app).listen(PORT, () => {
+    console.log(`Express server listening on port ` + PORT);
+});
